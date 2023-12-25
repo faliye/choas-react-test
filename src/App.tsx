eb50fd94d54c3f94d67d4824dffd8371abfb08ad
faliye <fy15309206903@gmail.com>
2023-12-25 22:45:03 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,36 @@ import Button from 'choas-react/src/Button';
 function App() {
   return (
     <div>
-      <Button type='primary' className='abc'>123</Button>
-      <Button type='danger' className='abc'>123</Button>
-      <Button type='ghost' className='abc'>123</Button>
-      <Button type='link' className='abc'>123</Button>
+      <div>
+        <Button size='large' type='primary' className='abc'>123</Button>
+        <Button size='large' type='danger' className='abc'>123</Button>
+        <Button size='large' type='ghost' className='abc'>123</Button>
+        <Button size='large' type='link' className='abc'>123</Button>
+      </div>
+      <br />
+      <div>
+        <Button type='primary' className='abc'>123</Button>
+        <Button type='danger' className='abc'>123</Button>
+        <Button type='ghost' className='abc'>123</Button>
+        <Button type='link' className='abc'>123</Button>
+      </div>
+      <br />
+      <div>
+        <Button size='small' type='primary' className='abc'>123</Button>
+        <Button size='small' type='danger' className='abc'>123</Button>
+        <Button size='small' type='ghost' className='abc'>123</Button>
+        <Button size='small' type='link' className='abc'>123</Button>
+      </div>
+      <br />
+      <div>
+        <Button type='primary' className='abc' block>123</Button>
+        <br />
+        <Button type='danger' className='abc' block>123</Button>
+        <br />
+        <Button type='ghost' className='abc' block>123</Button>
+        <br />
+        <Button type='link' className='abc' block>123</Button>
+      </div>
     </div>
   );
 }
